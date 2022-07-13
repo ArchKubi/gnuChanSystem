94fc4e582f6e9a1241dbe09404c462a7a69e02c2
ArchKubi <80429360+ArchKubi@users.noreply.github.com>
2022-07-13 16:36:30 +0300

    uwu

--- script/textEditor.gd
+++ script/textEditor.gd
@@ -4,10 +4,6 @@ onready var openFile = get_node("fileManager/OpenFile")
 onready var saveFile = get_node("fileManager/saveAsFile")
 onready var textEditor = get_node("MainWindow/bottomBar/TextEdit")
 
-var openFile_var
-var saveAsFile_var
-var current_file_path: String = ""
-
 
 func _process(_delta):
 	if gl.TextEditorShow == false:
@@ -24,15 +20,14 @@ func _on_saveas_pressed():
 	saveFile.popup_centered()
 
 func _on_OpenFile_file_selected(path):
-	openFile_var = File.new()
-	openFile_var.open(path,1)
-	current_file_path = path
-	textEditor.text = openFile_var.get_as_text()
+	var f = File.new()
+	f.open(path,1)
+	textEditor.text = f.get_as_text()
 
 func _on_saveAsFile_file_selected(path):
-	saveAsFile_var = File.new()
-	saveAsFile_var.open(path,2)
-	saveAsFile_var.store_string(textEditor.text)
+	var f = File.new()
+	f.open(path,2)
+	f.store_string(textEditor.text)
 
 
 func _on_savefile_pressed(path):
